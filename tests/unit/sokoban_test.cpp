f18244ff289c43bff89a3a701306f69be127fe28
Irvin <itancio@berkeley.edu>
2022-02-22 12:36:49 -1000

    Start sokoban test file

---
+++ tests/unit/sokoban_test.cpp
@@ -0,0 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "../../src/sokoban.hpp"
+
+// TEST_CASE("initial value should be 0") {
+//     Sokoban s = Sokoban();
+//     CHECK(c.solved() == 0);
+// };
