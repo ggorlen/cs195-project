5ff731e0216593b59d11841fb61e2aa7a62510ea
Irvin <itancio@berkeley.edu>
2022-02-22 14:25:48 -1000

    Create initial test

--- tests/unit/sokoban_test.cpp
+++ tests/unit/sokoban_test.cpp
@@ -2,7 +2,15 @@
 #include "doctest.h"
 #include "../../src/sokoban.hpp"
 
-// TEST_CASE("initial value should be 0") {
-//     Sokoban s = Sokoban();
-//     CHECK(c.solved() == 0);
-// };
+TEST_CASE("Default board should be the first board in levels") {
+    Sokoban s1 = Sokoban();
+    Sokoban s2 = Sokoban(levels[0]);
+    
+    CHECK(s1.board() == s2.board());
+};
+
+TEST_CASE("Two boards should not be the same") {
+    Sokoban s1 = Sokoban(2);
+    Sokoban s2 = Sokoban(levels[0]);
+    CHECK(s1.board() != s2.board());
+};
