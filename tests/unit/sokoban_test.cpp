0bb4ed6d4a056a1cdd44036c704787f5dd21c081
Irvin <itancio@berkeley.edu>
2022-02-22 18:46:59 -1000

    Revise the initial test

--- tests/unit/sokoban_test.cpp
+++ tests/unit/sokoban_test.cpp
@@ -2,15 +2,37 @@
 #include "doctest.h"
 #include "../../src/sokoban.hpp"
 
-TEST_CASE("Default board should be the first board in levels") {
-    Sokoban s1 = Sokoban();
-    Sokoban s2 = Sokoban(levels[0]);
+TEST_CASE("returns the board") {
+    std::vector<std::string> level = {
+        "#####",
+        "#@$.#",
+        "#####"
+    };
+    Sokoban soko = Sokoban();
     
-    CHECK(s1.board() == s2.board());
+    CHECK(soko.board() == level);
 };
 
-TEST_CASE("Two boards should not be the same") {
-    Sokoban s1 = Sokoban(2);
-    Sokoban s2 = Sokoban(levels[0]);
-    CHECK(s1.board() != s2.board());
-};
+// TEST_CASE("change_level throws when the level is out of bounds") {
+//     const std::vector<std::string> level{}; 
+//     Sokoban soko = Sokoban(); 
+    
+//     CHECK_THROWS_AS(soko.change_level(1), const std::exception&); }
+// };
+
+// TEST_CASE("pushes a box right onto a goal square") {
+//     std::vector<std::string> level = {
+//         "#####",
+//         "#@$.#",
+//         "#####"
+//     };
+
+//     std::vector<std::string> expected = {
+//         "#####",
+//         "# @*#",
+//         "#####",
+//     };
+//     Sokoban soko = Sokoban(2);
+//     soko.move(Sokoban::Direction::R);
+//     CHECK(soko.board() == expected);
+// };
